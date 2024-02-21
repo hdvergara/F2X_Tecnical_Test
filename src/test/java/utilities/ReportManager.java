ab431568d24c3bbe838f2e866ff566e7eab5fc83
Hector Vergara <hector-19-20@hotmail.com>
2024-02-21 06:53:36 -0500

    java docs are added to missing classes

--- src/test/java/utilities/ReportManager.java
+++ src/test/java/utilities/ReportManager.java
@@ -9,24 +9,38 @@ import java.time.format.DateTimeFormatter;
 
 public class ReportManager {
     private static ExtentReports extent;
-    private static ExtentSparkReporter htmlReporter;
     private static final String REPORT_PATH = "reports/";
     private static final String NAME_REPORT = "ExtentReport";
 
+    /**
+     * Method for initializing the reporter
+     */
     public static void initializeReport() {
-        htmlReporter = new ExtentSparkReporter(REPORT_PATH + NAME_REPORT + dateFormat() + ".html");
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(REPORT_PATH + NAME_REPORT + dateFormat() + ".html");
         extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
     }
 
+    /**
+     * Method for assigning a name to the test case in the report
+     * @param testName Test case name
+     * @return The name of the test case in the report
+     */
     public static ExtentTest createTest(String testName) {
         return extent.createTest(testName);
     }
 
+    /**
+     * Method that completes report generation
+     */
     public static void flushReport() {
         extent.flush();
     }
 
+    /**
+     * Method for capturing the current system date and time
+     * @return System date and time in String format
+     */
     private static String dateFormat(){
         LocalDateTime localDateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
